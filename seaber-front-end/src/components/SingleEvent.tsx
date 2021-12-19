97c02bc710c8a44cad7c9b5f3bdc38f32d8de57b
DLuu <luutuandungti@yahoo.com>
2021-12-19 17:47:05 +0200

    timeformat now work!

--- seaber-front-end/src/components/SingleEvent.tsx
+++ seaber-front-end/src/components/SingleEvent.tsx
@@ -7,6 +7,19 @@ function SingleEvent({ status }: { status: Status }) {
     color: 'white'
   };
 
+  const timeformat = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour12: false } as Intl.DateTimeFormatOptions;
+
+  const timeStart = new Date(status.start)
+    .toLocaleTimeString(
+      'en-US',
+      timeformat);
+
+  const timeEnd = new Date(status.start + status.duration)
+    .toLocaleTimeString(
+      'en-US',
+      timeformat);
+
+
   switch (status.event) {
     case 'Idle':
     case 'Ballast':
@@ -30,14 +43,8 @@ function SingleEvent({ status }: { status: Status }) {
       <td>{status.event}</td>
       <td>{status.portName}</td>
       <td>{status.orderId}</td>
-      <td>{new Date(status.start)
-        .toLocaleTimeString(
-          'en-US',
-          { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour12: false })}</td>
-      <td>{new Date(status.start + status.duration)
-        .toLocaleTimeString(
-          'en-US',
-          { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour12: false })}</td>
+      <td>{timeStart}</td>
+      <td>{timeEnd}</td>
       <td>{status.duration / 3600000}</td>
     </tr>
   )
